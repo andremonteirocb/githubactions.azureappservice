b3760fd87e90d409b6cc91fdc9c20efd17ec502c
André Monteiro <andremonteiro.cb@hotmail.com>
2022-09-22 00:01:23 -0300

    subindo projeto de estudo;

---
+++ Extensions/AssemblyExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace GithubActions.AzureAppService.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static Assembly entryAssembly = Assembly.GetExecutingAssembly();
+        public static void AddMapDefault(this WebApplication app)
+        {
+            app.MapControllers();
+            app.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync($"Container ({DateTime.Now}) - Update: {new FileInfo(entryAssembly.Location).LastWriteTime}");
+            });
+        }
+    }
+}
